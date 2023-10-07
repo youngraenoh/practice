c5e9cce56492b1484a402517bed1ad82aa3f8782
YoungRae Noh <ray8206@naver.com>
2023-10-07 12:49:45 +0900

    first commit

---
+++ theater_module.py
@@ -0,0 +1,8 @@
+def price(people) :
+    print("{0}명 가격은 {1}원입니다.".format(people, people * 10000))
+
+def price_morning(people) :
+    print("{0}명 조조할인 가격은 {1}원입니다.".format(people, people * 6000))
+
+def price_soldier(people) :
+    print("{0}명 조조할인 가격은 {1}원입니다.".format(people, people * 4000))
